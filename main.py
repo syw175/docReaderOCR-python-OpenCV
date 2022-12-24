222faab5826b5a1d260a2059cd737d8deeabb605
Steven Wong <syw14@sfu.ca>
2022-12-24 15:02:50 -0800

    reorganize into main()

--- main.py
+++ main.py
@@ -6,6 +6,50 @@
 import cv2 as cv
 import numpy as np
 
+def main():
+    # Read our receipt file as an image
+    img = cv.imread('receipt.png')
+    cv.imshow('Original', img)
+    cv.waitKey(0)
+
+    # Make a copy of the image for pre-processing
+    img_copy = img.copy()
+    # Remove the text from the image
+    img_copy = removeText(img_copy)
+    img_copy = grabCut(img_copy)
+    cv.imshow('Pre-Processing', img_copy)
+    cv.waitKey(0)
+
+    # Apply Gaussian Blur to the image to reduce noise in the image
+    img_copy = cv.GaussianBlur(img_copy, (11, 11), 0)
+    # Use the Canny Edge Detection algorithm to detect the edges of the document
+    img_copy = cv.cvtColor(img_copy, cv.COLOR_BGR2GRAY)
+    img_copy = cv.Canny(img_copy, 0, 200)
+    # Dilate the image to make the edges thinner
+    img_copy = cv.dilate(img_copy, cv.getStructuringElement(cv.MORPH_ELLIPSE, (5, 5)))
+    cv.imshow('Canny Edge Detection', img_copy)
+    cv.waitKey(0)
+
+    # Perform contour detection to get the contours of these edges and sort them by area, plot onto blank canvas
+    canvas = np.zeros_like(img)
+    # Find the contours of the image
+    contours, hierarchy = cv.findContours(img_copy, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+    # Sort the contours by area
+    contours = sorted(contours, key=cv.contourArea, reverse=True)[:5]
+    # Draw the contours onto the canvas
+    cv.drawContours(canvas, contours, -1, (0, 255, 0), 2)
+    cv.imshow('Contours', canvas)
+    cv.waitKey(0)
+
+    # Use the Douglas-Peucker algorithm to approximate the corners of the document
+    
+    # OCR the text: Once you have transformed the image, you can use Optical Character Recognition (OCR) to extract the text from the document. 
+    # There are several OCR libraries available for Python, such as Tesseract and pytesseract.
+
+    # Post-process the text: Finally, you may need to post-process the text to correct any errors or inconsistencies introduced by the OCR process.
+    #  This may include tasks such as spelling correction and layout analysis.
+    return
+
 # Use a series of erosion and dilation operations remove text for edge detection
 def removeText(image):
     kernel = np.ones((5,5),np.uint8)
@@ -25,34 +69,5 @@ def grabCut(image):
     image = image*mask2[:,:,np.newaxis]
     return image
 
-# Read our receipt file as an image
-img = cv.imread('receipt.png')
-cv.imshow('Original', img)
-cv.waitKey(0)
-
-# Make a copy of the image for pre-processing
-img_copy = img.copy()
-# Remove the text from the image
-img_copy = removeText(img_copy)
-img_copy = grabCut(img_copy)
-cv.imshow('Pre-Processing', img_copy)
-cv.waitKey(0)
-
-# Apply Gaussian Blur to the image to reduce noise in the image
-img_copy = cv.GaussianBlur(img_copy, (11, 11), 0)
-# Use the Canny Edge Detection algorithm to detect the edges of the document
-img_copy = cv.cvtColor(img_copy, cv.COLOR_BGR2GRAY)
-img_copy = cv.Canny(img_copy, 0, 200)
-# Dilate the image to make the edges thinner
-img_copy = cv.dilate(img_copy, cv.getStructuringElement(cv.MORPH_ELLIPSE, (5, 5)))
-cv.imshow('Canny Edge Detection', img_copy)
-cv.waitKey(0)
-
-# Perform contour detection to get the contours of these edges and sort them by area
-
-
-# OCR the text: Once you have transformed the image, you can use Optical Character Recognition (OCR) to extract the text from the document. 
-# There are several OCR libraries available for Python, such as Tesseract and pytesseract.
-
-# Post-process the text: Finally, you may need to post-process the text to correct any errors or inconsistencies introduced by the OCR process.
-#  This may include tasks such as spelling correction and layout analysis.
+if __name__ == "__main__":
+    main()
